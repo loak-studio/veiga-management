743fdd50ff70300f21804533dc289d36cefd5f99
Benjamin Geets <bge@tuta.io>
2023-03-23 08:32:32 +0100

    build: use Astro 😔

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-module.exports = (eleventyConfig) =>{
-  eleventyConfig.addWatchTarget("./src/styles/");
-  eleventyConfig.addPassthroughCopy({"public":"/"});
-  return{
-    dir:{
-      input:'src'
-    }
-  }
-}

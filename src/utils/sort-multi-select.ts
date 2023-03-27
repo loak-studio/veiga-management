37a5dc4364e55dcbce5fecd1aa91fb453fd418f4
lavaqq <mlava@tuta.io>
2023-03-27 09:55:35 +0200

    fix(utils): only return wanted res

--- src/utils/sort-multi-select.ts
+++ src/utils/sort-multi-select.ts
@@ -1,7 +1,10 @@
 export const sortLinks = (arr: string[], rels: Object[]) => {
-    return rels.sort((a: any, b: any) => {
-        const index1 = arr.indexOf(a.uuid);
-        const index2 = arr.indexOf(b.uuid);
-        return index1 - index2;
-    });
+    const sorted = rels
+        .filter((rel) => arr.includes(rel.uuid))
+        .sort((a: any, b: any) => {
+            const index1 = arr.indexOf(a.uuid);
+            const index2 = arr.indexOf(b.uuid);
+            return index1 - index2;
+        });
+    return sorted;
 };

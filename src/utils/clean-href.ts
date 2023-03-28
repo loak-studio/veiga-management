c21a0d15d51246c29e4dc9b021d32bbf35e93ca8
Benjamin Geets <bge@tuta.io>
2023-03-28 11:22:45 +0200

    refactor: create cleanHref function to handle slugs

---
+++ src/utils/clean-href.ts
@@ -0,0 +1,17 @@
+const cleanHref = (slug:string)=>{
+  let href = ""
+  href = slug
+  if(slug === "home"){
+    href = "/"
+    return href
+  }
+  if(href.slice(-1) !=="/"){
+    href = href + "/"
+  }
+  if(href.slice(0) !== "/"){
+    href = "/"+href
+  }
+  return href
+}
+
+export {cleanHref}

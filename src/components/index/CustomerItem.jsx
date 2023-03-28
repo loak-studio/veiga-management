fa97cb194cd45fba07e49fae7fadc7b6baf74455
Benjamin Geets <bge@tuta.io>
2023-03-28 11:52:44 +0200

    feat(customer-item): resize image

--- src/components/index/CustomerItem.jsx
+++ src/components/index/CustomerItem.jsx
@@ -4,7 +4,7 @@ export default function CustomerItem(props) {
   return (
     <li {...storyblokEditable(props.blok)} className="customer-item">
       <figure className="customer-item__figure">
-        <img src={props.blok.picture.filename} alt="" />
+        <img width={208} height={208} src={props.blok.picture.filename + "/m/208x208"} alt="" />
       </figure>
       <h3 className="customer-item__name h5">{props.blok.name}</h3>
       <span className="customer-item__role text">{props.blok.role}</span>

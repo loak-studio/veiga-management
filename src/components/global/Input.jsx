8d76c7a4b04b8e53737c9451076a2042cce4416c
Benjamin Geets <bge@tuta.io>
2023-03-28 11:02:07 +0200

    feat(input): add required

--- src/components/global/Input.jsx
+++ src/components/global/Input.jsx
@@ -2,16 +2,16 @@ export default function Input({name,id,label,type="text", required}){
  if(type=="textarea"){
   return(
     <div className="input">
-      <label className="input__label text" htmlFor={id}>{label} {required ? '*':''}</label>
-      <textarea className="input__input" name={name} id={id} rows="2"></textarea>
+      <label className="input__label text" htmlFor={id}>{label}</label>
+      <textarea required={required} className="input__input" name={name} id={id} rows="2"></textarea>
     </div>
   )
  }
  else{
   return(
     <div className="input">
-      <label className="input__label text" htmlFor={id}>{label} {required ? '*':''}</label>
-      <input className="input__input" id={id} name={name} type={type} />
+      <label className="input__label text" htmlFor={id}>{label}</label>
+      <input required={required} className="input__input" id={id} name={name} type={type} />
     </div>)
  }
 }

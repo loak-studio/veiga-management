851158546f4b5950f33785d47b5c07f553e66f05
Benjamin Geets <bge@tuta.io>
2023-03-30 13:34:11 +0200

    refactor(video): react syntax

--- src/components/global/Video.jsx
+++ src/components/global/Video.jsx
@@ -11,6 +11,6 @@ export default function Video(props){
   const descriptionHtml = renderRichText(props.blok.description)
   return(<div className="video" {...storyblokEditable(props.blok)}>
     <h2 className="video__title h2" dangerouslySetInnerHTML={{__html:titleHtml}}/>
-    <iframe className="video__iframe" src={"https://www.youtube.com/embed/" + youtube_parser(props.blok.youtubeLink.url)} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe className="video__iframe" src={"https://www.youtube.com/embed/" + youtube_parser(props.blok.youtubeLink.url)} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>)
 }

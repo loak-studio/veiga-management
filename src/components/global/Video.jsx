6e90635554da33d9d44f72ef81da83fa8d956d7f
Benjamin Geets <bge@tuta.io>
2023-03-30 12:57:16 +0200

    feat(video): parse youtube url

--- src/components/global/Video.jsx
+++ src/components/global/Video.jsx
@@ -1,9 +1,16 @@
 import { renderRichText, storyblokEditable } from "@storyblok/react"
 export default function Video(props){
+
+  function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
+
   const titleHtml = renderRichText(props.blok.title)
   const descriptionHtml = renderRichText(props.blok.description)
   return(<div className="video" {...storyblokEditable(props.blok)}>
     <h2 className="video__title h2" dangerouslySetInnerHTML={{__html:titleHtml}}/>
-    <iframe className="video__iframe" src="https://www.youtube.com/embed/aqVTzT2qH98" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe className="video__iframe" src={"https://www.youtube.com/embed/" + youtube_parser(props.blok.youtubeLink.url)} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>)
 }

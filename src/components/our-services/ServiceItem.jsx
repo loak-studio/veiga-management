0f4199f39c9a603d1cc2c10cef6e314e13e6f922
Benjamin Geets <bge@tuta.io>
2023-03-30 09:28:04 +0200

    feat(services): icons

--- src/components/our-services/ServiceItem.jsx
+++ src/components/our-services/ServiceItem.jsx
@@ -1,8 +1,142 @@
 import { storyblokEditable, renderRichText } from "@storyblok/react"
+
+const Facebook = ()=>{
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      fill="none"
+      viewBox="0 0 32 33"
+    >
+      <path
+        fill="currentColor"
+        d="M18.627 29.45V17.79h3.91l.593-4.553h-4.503v-2.903c0-1.32.363-2.21 2.26-2.21h2.408V4.049a33.945 33.945 0 00-3.514-.182c-3.48 0-5.855 2.128-5.855 6.02v3.35H10v4.552h3.926v11.678h4.7v-.016z"
+      ></path>
+    </svg>
+  );
+}
+
+const Instagram = ()=>{
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      fill="none"
+      viewBox="0 0 32 33"
+    >
+      <path
+        fill="currentColor"
+        d="M16 9.573a6.558 6.558 0 00-6.56 6.56 6.558 6.558 0 006.56 6.56 6.558 6.558 0 006.56-6.56c0-3.625-2.94-6.56-6.56-6.56zm0 10.818a4.26 4.26 0 11.002-8.518A4.26 4.26 0 0116 20.391z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="M21.28 3.413c-2.356-.11-8.191-.105-10.546 0-2.075.1-3.9.6-5.376 2.07-2.46 2.466-2.145 5.78-2.145 10.646 0 4.98-.275 8.22 2.145 10.645 2.47 2.47 5.836 2.145 10.646 2.145 4.93 0 6.635.005 8.38-.67 2.37-.92 4.16-3.04 4.336-6.845.11-2.355.105-8.19 0-10.546-.21-4.495-2.626-7.22-7.44-7.445zm3.724 21.73c-1.615 1.616-3.855 1.47-9.035 1.47-5.335 0-7.47.08-9.036-1.484-1.795-1.79-1.47-4.66-1.47-9.015 0-5.896-.605-10.14 5.31-10.44 1.36-.05 1.76-.066 5.18-.066l.05.03c5.686 0 10.146-.595 10.411 5.32.06 1.35.075 1.756.075 5.17 0 5.27.1 7.426-1.485 9.016z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="M22.8 10.853a1.52 1.52 0 100-3.04 1.52 1.52 0 000 3.04z"
+      ></path>
+    </svg>
+  );
+}
+
+
+const Linkedin = ()=>{
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      fill="none"
+      viewBox="0 0 32 33"
+    >
+      <path
+        fill="currentColor"
+        d="M26.11 3.867H7.49A3.493 3.493 0 004 7.357v18.62a3.493 3.493 0 003.49 3.49h18.62a3.49 3.49 0 003.49-3.49V7.357a3.493 3.493 0 00-3.49-3.49zm-13.385 20.26c0 .3-.24.54-.54.54h-2.3c-.3 0-.54-.24-.54-.54v-9.64c0-.3.24-.54.54-.54h2.3c.3 0 .54.245.54.54v9.64zm-1.69-11.095a2.183 2.183 0 010-4.365 2.183 2.183 0 010 4.365zm13.715 11.13c0 .275-.22.495-.495.495h-2.48a.493.493 0 01-.495-.495v-4.52c0-.675.2-2.955-1.765-2.955-1.52 0-1.83 1.56-1.89 2.26v5.215c0 .27-.22.495-.49.495h-2.39a.493.493 0 01-.495-.495v-9.72c0-.275.22-.495.495-.495h2.39c.275 0 .495.225.495.495v.84c.565-.85 1.4-1.5 3.185-1.5 3.955 0 3.925 3.69 3.925 5.72l.01 4.66z"
+      ></path>
+    </svg>
+  );
+}
+
+const Tiktok = ()=>{
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      fill="none"
+      viewBox="0 0 32 33"
+    >
+      <path
+        fill="currentColor"
+        d="M21.075 3.333c.137.55.137 1.1.275 1.65.688 2.338 2.613 4.127 5.09 4.402l.55.137v3.301c0 .688 0 .825-.688.688-1.651-.138-3.302-.688-4.814-1.513-.138 0-.276-.138-.413-.138v8.253c0 2.613-1.1 5.226-3.163 6.877-1.514 1.375-3.714 2.063-5.777 1.926-3.026-.276-5.64-2.339-6.465-5.227-.275-.963-.412-1.926-.55-2.889a8.128 8.128 0 015.914-7.84c.688-.137 1.376-.274 2.063-.274h.826v3.713c0 .275-.138.55-.413.688-.688.137-1.375.413-2.063.688-1.788.55-2.75 2.613-2.2 4.401l.412.825c.825 1.376 2.476 2.2 4.126 1.926 1.65-.413 2.751-1.788 2.889-3.439.137-1.1.137-2.063.137-3.163V3.745l4.264-.412z"
+      ></path>
+    </svg>
+  );
+}
+
+const Twitch = ()=>{
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      fill="none"
+      viewBox="0 0 32 33"
+    >
+      <path
+        fill="currentColor"
+        d="M22.07 10.013h-2.23v6.68h2.23v-6.68zM15.94 10.013h-2.23v6.68h2.23v-6.68z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="M5.36 3.333l-1.68 4.45v17.81h6.13v3.34h3.35l3.34-3.34h5.02l6.69-6.68.01-15.58H5.36zm20.62 14.48l-3.9 3.88h-6.14l-3.34 3.34v-3.34H7.58V5.553h18.4v12.26z"
+      ></path>
+    </svg>
+  );
+}
+
+const Twitter = ()=>{
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      fill="none"
+      viewBox="0 0 32 33"
+    >
+      <path
+        fill="currentColor"
+        d="M19.155 5.863c-2.32.87-3.77 3.046-3.626 5.367v.87l-.87-.145c-3.336-.435-6.237-1.886-8.848-4.352l-1.015-1.16-.435.87c-.726 2.031-.29 4.062 1.16 5.512.725.725.58.87-.725.435-.435-.145-.87-.29-.87-.145s.435 1.886.725 2.466c.435.87 1.45 1.886 2.465 2.32l.87.436H6.972c-1.015 0-1.015 0-1.015.435.435 1.16 1.886 2.466 3.481 3.046l1.16.435-1.015.58c-1.45.87-3.19 1.306-4.931 1.45-.87 0-1.45.146-1.45.146 0 .145 2.32 1.16 3.625 1.595 3.917 1.16 8.558.726 12.04-1.45 2.465-1.45 4.93-4.352 6.091-7.253.58-1.45 1.305-4.206 1.305-5.656 0-.87 0-1.016 1.16-2.03.581-.581 1.161-1.306 1.306-1.451.145-.29.145-.29-.725 0-1.74.725-1.886.725-1.16-.145.58-.58 1.305-1.74 1.305-2.031 0 0-.29 0-.58.145a9.568 9.568 0 01-1.596.58l-1.015.29-.87-.58c-.435-.29-1.16-.725-1.596-.87-1.015 0-2.465 0-3.336.29z"
+      ></path>
+    </svg>
+  );
+}
+
+const Youtube = ()=>{
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      fill="none"
+      viewBox="0 0 32 33"
+    >
+      <path
+        fill="currentColor"
+        d="M28.264 9.979a3.213 3.213 0 00-2.258-2.258c-2-.548-10.014-.548-10.014-.548s-8.009 0-10.014.528A3.276 3.276 0 003.72 9.98c-.52 2-.52 6.155-.52 6.155s0 4.175.528 6.156a3.213 3.213 0 002.257 2.258c2.025.548 10.014.548 10.014.548s8.01 0 10.014-.528a3.213 3.213 0 002.258-2.258c.528-2 .528-6.155.528-6.155s.012-4.175-.536-6.176zm-14.821 9.99v-7.673l6.664 3.838-6.664 3.835z"
+      ></path>
+    </svg>
+  );
+}
+
+
+
 export default function ServiceItem(props) {
   const descriptionHtml = renderRichText(props.blok.description)
   const testimonialHtml = renderRichText(props.blok.testimonial)
-  console.log(props.blok?.titleBackgroundColor?.color)
   return <div style={{ background: props.blok.backgroundTransparent ? null : props.blok.backgroundColor.color }} className="service-item" {...storyblokEditable(props.blok)}>
     <figure className="service-item__background-image">
       <img src={props.blok.backgroundImage?.filename} alt="" />
@@ -17,10 +151,13 @@ export default function ServiceItem(props) {
           <span className="service-item__customer-role h6">
             {props.blok.role}
           </span>
-          <ul className="service-item__links" style={{borderColor: props.blok?.textColor?.color}}>
-            <li>
-              <a target={"_blank"} href={props.blok.twitter.url}>twitter</a>
-            </li>
+          <ul className="service-item__links" style={{borderColor: props.blok?.textColor?.color, color:props.blok?.textColor?.color}}>
+            {props.blok.twitter?.url && <ServiceItemSocialLink link={props.blok.twitter}/>}
+            {props.blok.tiktok?.url && <ServiceItemSocialLink link={props.blok.tiktok}/>}
+            {props.blok.twitch?.url && <ServiceItemSocialLink link={props.blok.twitch}/>}
+            {props.blok.instagram?.url && <ServiceItemSocialLink link={props.blok.instagram}/>}
+            {props.blok.facebook?.url && <ServiceItemSocialLink link={props.blok.facebook}/>}
+            {props.blok.youtube?.url && <ServiceItemSocialLink link={props.blok.youtube}/>}
           </ul>
           <span className="service-item__followers text bold">
             {props.blok.followers}
@@ -35,3 +172,29 @@ export default function ServiceItem(props) {
     </div>
   </div>
 }
+
+
+function ServiceItemSocialLink(props){
+  let domain = (new URL(props.link.cached_url))
+  domain = domain.hostname
+  console.log(domain)
+  return(
+    <li className="social-item__link">
+      <a href={props.link.cached_url}>
+        {domainToIcon[domain]}
+      </a>
+    </li>
+  )
+}
+
+const domainToIcon = {
+  "twitter.com":<Twitter/>,
+  "linkedin.com":<Linkedin/>,
+  "youtube.com":<Youtube/>,
+  "youtu.be":<Youtube/>,
+  "instagram.com":<Instagram/>,
+  "facebook.com":<Facebook/>,
+  "tiktok.com":<Tiktok/>,
+  "twitch.tv":<Twitch/>
+}
+

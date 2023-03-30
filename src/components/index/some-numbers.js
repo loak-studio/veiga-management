0c2daa996b9e753951087cffc560334625740c90
Benjamin Geets <bge@tuta.io>
2023-03-30 12:17:17 +0200

    fix(index): set 0 to counters

--- src/components/index/some-numbers.js
+++ src/components/index/some-numbers.js
@@ -2,6 +2,9 @@ import { inView } from "motion"
 import { CountUp } from "countup.js";
 const launchCounter = ()=>{
   const counters = document.querySelectorAll('.value');
+  counters.forEach(counter=>{
+    counter.innerHTML = "0"
+  })
   inView('.some-numbers',()=>{
     counters.forEach( counter => {
       const con = new CountUp(counter, counter.getAttribute('akhi'), {duration:2})

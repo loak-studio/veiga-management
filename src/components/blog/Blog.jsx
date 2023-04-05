e3c04d11897061a4c2aa018a50c11ac2b6f4120d
Benjamin Geets <bge@tuta.io>
2023-04-05 12:58:39 +0200

    feat: prevent fetching draft stories

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -4,7 +4,7 @@ let totalEntries;
 const data = await fetch(
   "https://api.storyblok.com/v2/cdn/stories?starts_with=blog/&is_startpage=0&sort_by=content.date:desc&page=1&per_page=8&cv=1680250222&token=" +
     import.meta.env.PUBLIC_STORYBLOK_TOKEN +
-    "&version=draft"
+    "&version=" + (import.meta.env.DEV ? 'draft':'published')
 ).then((response) => {
   totalEntries = new Headers(response.headers).get("total");
   return response.json();

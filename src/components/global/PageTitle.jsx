d0a9c9b33c58f2fe14662c59f7204986cfb9b7cd
Benjamin Geets <bge@tuta.io>
2023-04-05 12:29:05 +0200

    feat(page-title): buttons & description

--- src/components/global/PageTitle.jsx
+++ src/components/global/PageTitle.jsx
@@ -1,4 +1,4 @@
-import { storyblokEditable, renderRichText } from "@storyblok/react";
+import { storyblokEditable, renderRichText, StoryblokComponent } from "@storyblok/react";
 export default function PageTitle(props) {
   const __html = renderRichText(props.blok.title);
   return (
@@ -10,6 +10,12 @@ export default function PageTitle(props) {
         className="page-title__title h1"
         dangerouslySetInnerHTML={{ __html }}
       />
+      {props.blok.description && <p className="page-title__description text">{props.blok.description}</p>}
+      {props.blok.buttons && 
+        <div className="page-title__buttons">
+          {props.blok.buttons.map((button)=><StoryblokComponent key={button._uid} blok={button}/>)}
+        </div>
+      }
     </div>
   );
 }

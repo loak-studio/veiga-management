f9bbb7e03eb6f25345b904dcd3f8e44047803d0f
Benjamin Geets <bge@tuta.io>
2023-04-05 10:35:48 +0200

    fix(numbers): use list item

--- src/components/index/SomeNumbersItem.jsx
+++ src/components/index/SomeNumbersItem.jsx
@@ -4,11 +4,11 @@ export default function SomeNumbersItem(props) {
   const items = props.blok.number.match(/[^0-9]/g)
   let numberValue = props.blok.number.replace(/[^0-9]/g, ''); 
   return (
-    <div className="some-numbers-item" {...storyblokEditable(props.blok)}>
+    <li className="some-numbers-item" {...storyblokEditable(props.blok)}>
       <span className="some-numbers-item__number h1">{items?.length > 0 && items[0]}<span akhi={numberValue} className="value">{numberValue}</span>{items?.length > 1 && items[1]}</span>
       <span className="some-numbers-item__label h6">
         <span>{props.blok.label}</span>
       </span>
-    </div>
+    </li>
   );
 }

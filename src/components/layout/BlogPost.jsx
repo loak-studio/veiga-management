1bb02679f42d20538e01fbefebe640add0dbd204
Benjamin Geets <bge@tuta.io>
2023-04-05 13:55:23 +0200

    feat(blog): richtext caption & image with caption

--- src/components/layout/BlogPost.jsx
+++ src/components/layout/BlogPost.jsx
@@ -3,7 +3,25 @@ import Container from "../layout/ContainerBackgroundSpace.jsx";
 import { parseDateToFrench } from "../../utils/parse-date-to-french";
 export default function BlogPost(props) {
   const __html = renderRichText(props.blok.title);
-  const content__html = renderRichText(props.blok.content);
+  const content__html = renderRichText(props.blok.content,{
+    resolver:(component,blok)=>{
+      console.log(component)
+      switch (component) {
+        case "ImageWithCaption":
+          return `<figure class="image-with-caption text"><img src="${blok.image.filename}"/><figcaption>${blok.caption}</figcaption></figure>`
+        case "Caption":
+          return`
+            <figure class="caption">
+              <blockquote class="text">${blok.caption}</blockquote>
+              <figcaption class="text bold">${blok.author}</figcaption>
+            </figure>
+          `
+        default:
+          break;
+      }
+      return component
+    }
+  });
   return (
     <article>
       <Container blok={{ arrow: false, display_icons: true }}>

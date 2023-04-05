0b3f47984dafcf32af225ec8bb3f87cc6ebe648a
Benjamin Geets <bge@tuta.io>
2023-04-05 10:32:26 +0200

    fix(index): service list use list item

--- src/components/index/HeroServiceItem.jsx
+++ src/components/index/HeroServiceItem.jsx
@@ -4,7 +4,7 @@ import { renderRichText, storyblokEditable } from "@storyblok/react";
 export default function HeroServiceItem(props) {
   const __html = renderRichText(props.blok.title);
   return (
-    <div className="hero-service-item" {...storyblokEditable(props.blok)}>
+    <li className="hero-service-item" {...storyblokEditable(props.blok)}>
       <figure className="hero-service-item__figure">
         <img src={props.blok.icon.filename} alt="" />
       </figure>
@@ -12,6 +12,6 @@ export default function HeroServiceItem(props) {
         className="hero-service-item__text h6"
         dangerouslySetInnerHTML={{ __html }}
       />
-    </div>
+    </li>
   );
 }

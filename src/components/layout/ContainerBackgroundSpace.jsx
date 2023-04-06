ceedfaa3ebc117e2428fdcc09f85a0449693427e
Benjamin Geets <bge@tuta.io>
2023-04-06 08:43:06 +0200

    feat(container): webp icons

--- src/components/layout/ContainerBackgroundSpace.jsx
+++ src/components/layout/ContainerBackgroundSpace.jsx
@@ -12,72 +12,96 @@ import bigTwitter from "../../assets/hero/twitter_grand.png";
 import bigYoutube from "../../assets/hero/youtube_grand.png";
 import smallYoutube from "../../assets/hero/youtube_petit.png";
 
+import smallFacebookWebp from "../../assets/hero/facebook_petit.webp";
+import bigInstagramWebp from "../../assets/hero/instagram_grand.webp";
+import bigPinterestWebp from "../../assets/hero/pinterest_grand.webp";
+import rightSmartphoneWebp from "../../assets/hero/smartphone_droite.webp";
+import leftSmartphoneWebp from "../../assets/hero/smartphone_gauche.webp";
+import bigTiktokWebp from "../../assets/hero/tiktok_grand.webp";
+import smallTiktokWebp from "../../assets/hero/tiktok_petit.webp";
+import smallTwitterWebp from "../../assets/hero/twitter_petit.webp";
+import bigTwitterWebp from "../../assets/hero/twitter_grand.webp";
+import bigYoutubeWebp from "../../assets/hero/youtube_grand.webp";
+import smallYoutubeWebp from "../../assets/hero/youtube_petit.webp";
+
+
 const images = [
   {
     className: "small-facebook from-right",
     asset: smallFacebook,
     width:135,
-    height:129
+    height:129,
+    webp:smallFacebookWebp
   },
   {
     className: "big-instagram from-right",
     asset: bigInstagram,
     width:177,
-    height:175
+    height:175,
+    webp:bigInstagramWebp
   },
   {
     className: "big-pinterest from-left",
     asset: bigPinterest,
     width:155,
-    height:150
+    height:150,
+    webp: bigPinterestWebp
   },
   {
     className: "right-smartphone from-right",
     asset: rightSmartphone,
     width:415,
-    height:568
+    height:568,
+    webp:rightSmartphoneWebp
   },
   {
     className: "left-smartphone from-left",
     asset: leftSmartphone,
     width:315,
-    height:358
+    height:358,
+    webp:leftSmartphoneWebp
   },
   {
     className: "big-tiktok from-right",
     asset: bigTiktok,
     width:230,
-    height:232
+    height:232,
+    webp:bigTiktokWebp
   },
   {
     className: "small-tiktok from-left",
     asset: smallTiktok,
     width:83,
-    height:82
+    height:82,
+    webp: smallTiktokWebp
   },
   {
     className: "small-twitter from-left",
     asset: smallTwitter,
     width:121,
-    height:123
+    height:123,
+    webp: smallTwitterWebp
   },
   {
     className: "big-twitter from-left",
     asset: bigTwitter,
     width:245,
-    height:243
+    height:243,
+    webp:bigTwitterWebp
   },
   {
     className: "big-youtube from-right",
     asset: bigYoutube,
     width:258,
-    height:274
+    height:274,
+    webp:bigYoutubeWebp
   },
   {
     className: "small-youtube from-left",
     asset: smallYoutube,
     width:116,
-    height:109
+    height:109,
+    webp: smallYoutubeWebp
   },
 ];
 
@@ -106,7 +130,10 @@ export default function Container(props) {
               key={index}
               className={"container__icon" + " " + image.className}
             >
+              <picture>
+              <source srcset={image.webp} type="image/webp" />
               <img width={image.width} height={image.height} src={image.asset} alt="" />
+              </picture>
             </figure>
           ))}
       </div>

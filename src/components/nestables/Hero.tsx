3aac3673b60816ff3d48d181205d62b64de4bc8d
Benjamin Geets <bge@tuta.io>
2023-04-28 16:23:26 +0200

    feat(hero): set to richtext to allow red class

--- src/components/nestables/Hero.tsx
+++ src/components/nestables/Hero.tsx
@@ -6,12 +6,13 @@ import {
 
 export default function Hero({ blok }: any) {
   const __html = renderRichText(blok.description);
+  const title__html = renderRichText(blok.title)
   return (
     <div className="hero" {...storyblokEditable(blok)}>
       <figure className="hero__logo">
         <img width={430} height={209} src={blok.logo.filename} alt="" />
       </figure>
-      <h1 className="h1 hero__title">{blok?.title}</h1>
+      <div className="h1 hero__title" dangerouslySetInnerHTML={{__html:title__html}}/>
       <div
         className="text hero__richtext richtext"
         dangerouslySetInnerHTML={{ __html }}
